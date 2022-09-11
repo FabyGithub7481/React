a607ffe1776771afa498d0046896736d4e704cc8
FabyGithub7481 <gabogabidia@hotmail.com>
2022-09-11 17:38:17 -0500

    primer commit

--- src/index.js
+++ src/index.js
@@ -1,18 +1,32 @@
 import React from "react";
 import ReactDom from "react-dom/client";
-import {Post} from './Post'
-
-
+import { Post } from "./Post";
 
 // una forma de seleccionar el div root el elemento root
 /* const rootElement= document.getElementById('root')
 ReactDom.createRoot(rootElement) */
 const root = ReactDom.createRoot(document.getElementById("root"));
 
+const users = [
+  {
+    id: 1,
+    name: "John",
+    email: "john@gmail.com",
+    image: "https://robohash.org/user1",
+  },
+  {
+    id: 2,
+    name: "Joe",
+    email: "joe@gmail.com",
+    image: "https://robohash.org/user2",
+  },
+];
 
 //fragment <> despues
 root.render(
   <>
-  <Post/>
+    {users.map((user,index) => {
+      return <h1 key={index}>{user.name}</h1>;
+    })}
   </>
 );
