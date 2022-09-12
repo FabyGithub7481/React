56ebcac3052bf87883bccadbd59a9190b8e1f508
FabyGithub7481 <gabogabidia@hotmail.com>
2022-09-11 19:06:55 -0500

    hooks useEfects

--- src/index.js
+++ src/index.js
@@ -1,29 +1,45 @@
-import React,{useState} from "react";
+import React, { useState, useEffect } from "react";
 import ReactDom from "react-dom/client";
-import { Post } from "./Post";
 
 // una forma de seleccionar el div root el elemento root
 /* const rootElement= document.getElementById('root')
 ReactDom.createRoot(rootElement) */
 const root = ReactDom.createRoot(document.getElementById("root"));
 function Counter() {
-  const [mensaje,setMensaje]= useState('')
+  const [mensaje, setMensaje] = useState("");
+  const [counter, setCounter] = useState(0);
+  useEffect(() => {
+    console.log("render");
+  }, [counter]);
+
+  
   return (
-<div>
-  {/* <input onChange={e => console.log(e.target.value)}/> */}
-  <input onChange={e => setMensaje(e.target.value)}/>
-  <button onClick={()=> {
-    alert('El mensaje es: '+mensaje)
-  }}>
-    Save
-  </button>
-</div>
+    <div>
+      <input onChange={(e) => setMensaje(e.target.value)} />
+      <button
+        onClick={() => {
+          alert("El mensaje es: " + mensaje);
+        }}
+      >
+        Save
+      </button>
+
+      <hr />
+      <h1>Counter : {counter}</h1>
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+        }}
+      >
+        Incrementar
+      </button>
+    </div>
   );
 }
 
 //fragment <> despues
 root.render(
   <>
-   <Counter />
+    <Counter />
   </>
 );
